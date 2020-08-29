80bcbb95d5e53026e7b00e399b773231e2e6dff3
MuhammadDarmawan007 <67378102+MuhammadDarmawan007@users.noreply.github.com>
2020-08-29 18:58:31 +0700

    Add files via upload

---
+++ responsi/app/src/main/java/com/example/responsi_mc_17030027/recyclerview_connect_api/config/ServerConfig.java
@@ -0,0 +1,6 @@
+package com.example.responsi_mc_17030027.recyclerview_connect_api.config;
+
+public class ServerConfig {
+    public static final String URL_BASE = "https://api.themoviedb.org";
+    public static final String API_ENDPOINT = URL_BASE;
+}
